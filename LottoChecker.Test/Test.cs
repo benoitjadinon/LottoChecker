318e583153324f52025fb78721b9dba9313d9d03
Benoit Jadinon <j@dinonbeno.it>
2017-01-15 16:43:32 +0100

    showing result + tests

---
+++ LottoChecker.Test/Test.cs
@@ -0,0 +1,112 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using Microsoft.ProjectOxford.Vision.Contract;
+
+namespace LottoChecker.Test
+{
+	[TestFixture()]
+	public class Test
+	{
+		[Test()]
+		public void TestHasAWinningLine()
+		{
+			var vm = new LottoCheckerViewModel();
+
+			var winningNumbers = new int[] { 18, 21, 26, 39, 42, 43 };
+			var playedLines = new List<int[]>
+			{
+				new int[] { 1, 2, 3, 4, 5, 6 },
+				new int[] { 1, 1, 26, 33, 42, 43 },
+			};
+
+			Assert.IsTrue(vm.IsTicketWinning(winningNumbers, playedLines));
+		}
+
+	    [Test()]
+	    public void TestHasNoWinningLine()
+	    {
+	        var vm = new LottoCheckerViewModel();
+
+	        var winningNumbers = new int[] { 18, 21, 26, 39, 42, 43 };
+	        var playedLines = new List<int[]>
+	        {
+	            new int[] { 1, 2, 3, 4, 5, 6 },
+	            new int[] { 9, 8, 7, 6, 5, 4 },
+	        };
+
+	        Assert.IsFalse(vm.IsTicketWinning(winningNumbers, playedLines));
+	    }
+
+	    [Test()]
+	    public void TestHasOnlyTwoNumbers()
+	    {
+	        var vm = new LottoCheckerViewModel();
+
+	        var winningNumbers = new int[] { 18, 21, 26, 39, 42, 43 };
+	        var playedLines = new List<int[]>
+	        {
+	            new int[] { 1, 2, 3, 4, 5, 6 },
+	            new int[] { 18, 26, 7, 6, 5, 4 },
+	        };
+
+	        Assert.IsFalse(vm.IsTicketWinning(winningNumbers, playedLines));
+	    }
+
+
+	    [Test()]
+	    public void TestExtractNumbers()
+	    {
+	        var vm = new LottoCheckerViewModel();
+
+	        var results = new OcrResults
+	        {
+	            Regions = new[]
+	            {
+	                new Region
+	                {
+	                    Lines = new []
+	                    {
+	                        new Line
+	                        {
+	                            Words = new []
+	                            {
+	                                new Word { Text = "01" },
+	                                new Word { Text = "02" },
+	                                new Word { Text = "03" },
+	                                new Word { Text = "04" },
+	                                new Word { Text = "05" },
+									new Word { Text = "06" },
+									new Word { Text = "07" },
+									new Word { Text = "08" },
+									new Word { Text = "09" },
+	                            }
+	                        },
+	                        new Line
+	                        {
+	                            Words = new []
+	                            {
+	                                new Word { Text = "01." },
+	                                new Word { Text = "18" },
+	                                new Word { Text = "21" },
+	                                new Word { Text = "26" },
+	                                new Word { Text = "39" },
+	                                new Word { Text = "42" },
+	                                new Word { Text = "43" },
+	                            }
+	                        },
+	                    }
+	                }
+	            }
+	        };
+
+	        var expectedLines = new List<int[]>
+	        {
+	            new [] { 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+	            new [] { 18, 21, 26, 39, 42, 43 },
+	        };
+
+	        Assert.AreEqual(expectedLines, vm.ExtractNumbers(results));
+	    }
+	}
+}
---
+++ LottoChecker/LottoCheckerViewModel.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Xml;
+using System.Xml.Linq;
+using Microsoft.ProjectOxford.Vision;
+using Microsoft.ProjectOxford.Vision.Contract;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using Xamarin.Forms;
+
+namespace LottoChecker
+{
+    public class LottoCheckerViewModel : INotifyPropertyChanged
+    {
+        private const string MicrosoftVisionAPIToken = "___________________________";
+
+        public ICommand ScanCommand { get; private set; }
+
+        private string _result = "";
+        public string Result 
+        { 
+            get { return _result; }
+            private set { 
+                _result = value;
+                NotifyPropertyChanged(nameof(Result));
+            } 
+        }
+
+        private void NotifyPropertyChanged(string propertyName)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private readonly VisionServiceClient _ocrClient;
+        private bool _isInitialized = false;
+
+
+        public LottoCheckerViewModel()
+        {
+            _ocrClient = new VisionServiceClient(MicrosoftVisionAPIToken);
+			
+            ScanCommand = new Command (async () => {
+                Result = "Loading...";
+                Result = await ScanAsync() ? "Winning ticket !" : "Lost.";
+            }, () => true);
+        }
+
+
+        private async Task<bool> ScanAsync()
+        {
+            var winningNumbers = await LoadResultsAsync();
+            var scannedNumberLines = await ScanTicketAsync();
+
+            return IsTicketWinning(winningNumbers, scannedNumberLines);
+        }
+
+        public bool IsTicketWinning(int[] winningNumbers, IEnumerable<int[]> scannedNumberLines)
+            => scannedNumberLines.Any(line => line.Count(winningNumbers.Contains) >= 3);
+
+        private Task<int[]> LoadResultsAsync()
+        {
+            return Task.Run(() =>
+            {
+                var xmlDoc = XDocument.Load("https://www.e-lotto.be/cache/dgLastResultForGameWithAddons/FR/Lotto6.xml");
+                return xmlDoc.Root
+                        .Element ("gameevent")
+                        .Element ("gamedraws")
+                        .Elements("gamedraw").First()
+                        .Element ("resultsets")
+                        .Elements("resultset").First()
+                        .Element ("mainvalues")
+                        .Value
+                        .Split(',')
+                        .Select(XmlConvert.ToInt32)
+                        .ToArray()
+                    ;
+            });
+        }
+
+        private async Task<IEnumerable<int[]>> ScanTicketAsync()
+        {
+            if (!_isInitialized)
+                _isInitialized = await CrossMedia.Current.Initialize();
+
+            MediaFile photo;
+            if (CrossMedia.Current.IsCameraAvailable)
+                photo = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+                {
+                    SaveToAlbum = false,
+                });
+            else
+                photo = await CrossMedia.Current.PickPhotoAsync();
+
+            using (var photoStream = photo.GetStream()){
+                //TODO : reduce photo before, because 413 req entity too large
+                return await RecognizeNumberLines(photoStream);
+            }
+
+            //return await Recognize("https://goo.gl/photos/PS6nHuH8Y7LW2oWc6");
+        }
+
+        private async Task<IEnumerable<int[]>> RecognizeNumberLines(Stream stream)
+        {
+            return ExtractNumbers(await _ocrClient.RecognizeTextAsync(stream));
+        }
+
+        private async Task<IEnumerable<int[]>> RecognizeNumberLines(string url)
+        {
+            return ExtractNumbers(await _ocrClient.RecognizeTextAsync(url));
+        }
+
+        public IEnumerable<int[]> ExtractNumbers(OcrResults ocrResult)
+        {
+            return
+                from region in ocrResult.Regions
+                from lines in region.Lines
+                where lines.Words.Length >= 6
+                select lines.Words.Where(word => IsNumeric(word.Text))
+                    .Select(word => word.Text)
+                    .Select(v => Convert.ToInt32(v))
+                    .ToArray()
+            ;
+        }
+
+        private readonly Func<string, bool> IsNumeric =
+            x => !string.IsNullOrEmpty(x) && x.ToCharArray().All(char.IsDigit);
+    }
+}
